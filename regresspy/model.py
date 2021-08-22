5e0d2e07d8bbe131cea2d975bdbc4beac09a67a5
HASNUR-JAHAN <hasnurjahan1997@gmail.com>
2021-08-22 11:46:08 -0700

    All the files updated

--- regresspy/model.py
+++ regresspy/model.py
@@ -1,4 +1,3 @@
-
 from sklearn.linear_model import SGDRegressor
 from sklearn.datasets import load_iris
 from regresspy.regression import Regression
@@ -10,14 +9,20 @@ X = iris.data[:, 0].reshape(-1, 1)
 Y = iris.data[:, 1].reshape(-1, 1)
 
 
+
+#TODO Perform a linear regression using sklearn and calculate training rmse.
+# Use the SGDRegressor and only select set learning rate and epochs.
+
 stochastic_gradient_descent = SGDRegressor(max_iter= 100, learning_rate= 'constant', eta0= 0.001)
 stochastic_gradient_descent.fit(X, Y.reshape(-1))
 sto_chas_grad_prediction = stochastic_gradient_descent.predict(X)
 sto_chas_grad_rmse = rmse(sto_chas_grad_prediction, Y)
 print('Stochastic Gradient Descent Regressor RMSE value:', str(sto_chas_grad_rmse))
 
+# #TODO Perform a linear regression using your code and calculate training rmse.
 
-regression_value = Regression(epochs= 100, learning_rate= 0.0001)
+
+regression_value = Regression(epochs= 20, learning_rate= 0.0001)
 regression_value.fit(X, Y)
 reg_pred = regression_value.predict(X)
 #print(reg_pred.shape)
